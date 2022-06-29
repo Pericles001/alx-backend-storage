0c8591b7cc61619e832bd331ebfe54fb3678d07c
pericles001 <periclesadjovi@outlook.fr>
2022-06-29 20:51:43 +0100

    Fix file

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,4 +2,4 @@
 -- Glam rock as their main style, ranked by their longevity
 SELECT band_name, COALESCE(split, 2020) - formed as lifespan 
 FROM metal_bands
-WHERE style LIKE '%Glam rock%';
+WHERE style like '%Glam rock%';
