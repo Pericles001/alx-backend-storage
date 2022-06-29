f1128fbc138f5aacb3f6c7f3f1ab7ffe261088a7
pericles001 <periclesadjovi@outlook.fr>
2022-06-29 20:28:49 +0100

    Update file

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- Script that creates a table users
-CREATE IF NOT EXISTS TABLE users (
-    id INT NOTNULL AUTOINCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOTNULL UNIQUE,
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOTNULL
+    country ENUM('US', 'CO', 'TN') NOT NULL
 );
