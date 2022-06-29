107051497c7e3924c1ace5a5cec64ab9b46b6b6d
pericles001 <periclesadjovi@outlook.fr>
2022-06-29 21:12:17 +0100

    Remove comment

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,4 @@
 --Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
---Quantity in the table items can be negative.
 CREATE TRIGGER decrement
 AFTER INSERT
 ON orders
