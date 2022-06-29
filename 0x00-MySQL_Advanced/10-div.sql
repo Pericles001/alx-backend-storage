e5dc15238c3327273ec80940b312770208d46888
pericles001 <periclesadjovi@outlook.fr>
2022-06-29 21:29:58 +0100

    Update file

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,6 +1,5 @@
---  creates a function SafeDiv that divides (and returns) 
--- the first by the second number or returns
---  0 if the second number is equal to 0.
+--  creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+
 DELIMITER //
 
 DROP FUNCTION IF EXISTS SafeDiv;
@@ -11,4 +10,5 @@ RETURNS FLOAT DETERMINISTIC
 BEGIN
     RETURN (IF (b = 0, 0, a / b));
 END //
+
 DELIMITER;
