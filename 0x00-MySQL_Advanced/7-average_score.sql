6b04055b9e207930ad951a42b02c4dd3ce9bfe09
pericles001 <periclesadjovi@outlook.fr>
2022-06-29 17:05:13 +0100

    Edit file -Solve mandatory task

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,12 @@
+-- a SQL script that creates a stored procedure
+-- ComputeAverageScoreForUser that computes
+-- and store the average score for a student
+DELIMITER $$
+CREATE PROCEDURE ComputerAverageScoreForUser (IN user_id INT)
+BEGIN
+    UPDATE users
+    SET
+    average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
+END $$
+DELIMITER;
