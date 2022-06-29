c687d5658b72b85592b84175d300f0b127a0ccb9
Pericles Adjovi <55035365+Pericles001@users.noreply.github.com>
2022-06-29 21:09:02 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
-WHERE style LIKE '%Glam rock%' ;
+SELECT band_name, IF(split is NULL , 2020 - formed, (split - formed))  lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
