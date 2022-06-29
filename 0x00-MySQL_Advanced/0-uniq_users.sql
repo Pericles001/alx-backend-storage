8c490b0c72eac687cbe1d379c1e7adce0117c102
pericles001 <periclesadjovi@outlook.fr>
2022-06-29 11:48:49 +0100

    Edit file

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Script that creates a table users
+CREATE IF NOT EXISTS TABLE users (
+    id INT NOTNULL AUTOINCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOTNULL UNIQUE,
+    name VARCHAR(255)
+);
