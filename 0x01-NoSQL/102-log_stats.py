bd95aee14b6e29e9a56b7ee793af8d47e9b4cf9a
pericles001 <periclesadjovi@outlook.fr>
2022-07-05 15:48:36 +0100

    Update file

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -5,9 +5,8 @@ Log stats
 from pymongo import MongoClient
 
 
-def log_stats(mongo_collection, option=None):
-    """
-    provides some stats about Nginx logs stored in MongoDB
+def log_stats():
+    """ log_stats.
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
@@ -18,8 +17,7 @@ def log_stats(mongo_collection, option=None):
     patch = logs_collection.count_documents({"method": "PATCH"})
     delete = logs_collection.count_documents({"method": "DELETE"})
     path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
+        {"method": "GET", "path": "/status"})
     print(f"{total} logs")
     print("Methods:")
     print(f"\tmethod GET: {get}")
@@ -28,6 +26,16 @@ def log_stats(mongo_collection, option=None):
     print(f"\tmethod PATCH: {patch}")
     print(f"\tmethod DELETE: {delete}")
     print(f"{path} status check")
+    print("IPs:")
+    sorted_ips = logs_collection.aggregate(
+        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+         {"$sort": {"count": -1}}])
+    i = 0
+    for s in sorted_ips:
+        if i == 10:
+            break
+        print(f"\t{s.get('_id')}: {s.get('count')}")
+        i += 1
 
 
 if __name__ == "__main__":
